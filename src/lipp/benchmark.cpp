31dc155c10539b106f01cc98fdf00e9de1b20257
Nicolau Pereira Alff <npalff@inf.ufrgs.br>
2022-06-27 13:43:33 +0200

    lognormal data type

--- src/lipp/benchmark.cpp
+++ src/lipp/benchmark.cpp
@@ -20,8 +20,11 @@
 #include <cassert>
 
 // Modify these if running your own workload
-#define KEY_TYPE double
-#define PAYLOAD_TYPE double
+//#define KEY_TYPE double
+//#define PAYLOAD_TYPE double
+
+#define KEY_TYPE int64_t
+#define PAYLOAD_TYPE int64_t
 
 /*
  * Required flags:
