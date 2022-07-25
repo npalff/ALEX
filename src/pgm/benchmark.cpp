a28993dce9aa2468cf5ed9e5d2623a50252f487e
Nicolau Pereira Alff <npalff@inf.ufrgs.br>
2022-07-25 12:02:03 +0200

    cut out payload usage

--- src/pgm/benchmark.cpp
+++ src/pgm/benchmark.cpp
@@ -132,12 +132,12 @@ int main(int argc, char* argv[]) {
         KEY_TYPE key = lookup_keys[j];
         
         //PAYLOAD_TYPE* payload = dynamic_pgm.find(key);
-        pgm::DynamicPGMIndex<KEY_TYPE, PAYLOAD_TYPE>::Iterator payload = dynamic_pgm.find(key);
+        auto payload = dynamic_pgm.find(key);
 
-        if (payload) {
-          sum += payload.K();   
+        //if (payload) {
+        //  sum += payload.K();   
         
-        }
+        //}
        
       }
       
