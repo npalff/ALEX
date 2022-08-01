250ae5bc0651c125fa49e91271eec63f9057e6d9
Nicolau Pereira Alff <npalff@inf.ufrgs.br>
2022-08-01 02:30:42 +0200

    add more DBs in createDB file

--- createDB.py
+++ createDB.py
@@ -1,6 +1,7 @@
 import sys
+import math
 
-INTMAX = sys.maxint()
+INTMAX = 2147483647
 numkeys = 0
 
 try:
@@ -9,22 +10,44 @@ try:
 except:
     print("Command: python createDB.py [experiment] [num keys]")
 
-
-
+numkeys=int(numkeys)
 
 dbFile1= open("../db/createdDB.txt",'w')
+#dbFile1= open("createdDB.txt",'w')
 
 if experiment=="minMax":
-    for i in range(numkeys):
+    for i in range(int(math.ceil(numkeys/2))):
         dbFile1.write(str(2*i+1)+"\n")
-        dbFile2.write(str(INTMAX-2*i)+"\n")
+    for i in range(int(math.ceil(numkeys/2))):
+        dbFile1.write(str(INTMAX-2*i)+"\n")
+
+
 elif experiment=="density":
-    print("hello")
+    for i in range(int(math.ceil(numkeys/2))):
+        dbFile1.write(str(2*i+1)+"\n")
+    for i in range(int(math.ceil(numkeys/2))):
+        dbFile1.write(str(2*i)+"\n")
+
 elif experiment=="primeNumbers":
-    print("hello")
+    arrayAux =[]
+    primeCounterAux = 0
+    i=1
+    while(primeCounterAux != numkeys and INTMAX>i):
+        if i>1:
+            for j in range(2,i):
+                print("i:" + str(i))
+                print("j:" + str(j)+"\n")
+                if(i%j == 0):
+                    break
+    
+            else:
+                primeCounterAux +=1
+                arrayAux.append(i)
+                dbFile1.write(str(i)+"\n")
+        i+=1
+
 else:
     "No experiment with this name"
-
 
 
 
