793c3c8115103cc5d3ca983f6f46894cfd2a47d8
Nicolau Pereira Alff <npalff@inf.ufrgs.br>
2022-09-26 13:52:55 +0200

    lipp debug keys

--- src/lipp/benchmark.cpp
+++ src/lipp/benchmark.cpp
@@ -92,8 +92,9 @@ int main(int argc, char* argv[]) {
   LIPP<KEY_TYPE, PAYLOAD_TYPE> lipp;
   std::sort(values, values + init_num_keys,
             [](auto const& a, auto const& b) { return a.first < b.first; });
-  for (int i = 1; i < init_num_keys; i ++){
-    std::cout<< values[i];
+  for (int k = 1; k < init_num_keys; k++)
+  {
+    std::cout<< values[k].first;
   }
   lipp.bulk_load(values, init_num_keys);
 
